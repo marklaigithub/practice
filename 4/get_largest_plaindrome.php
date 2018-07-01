34cc24115a3cc077c893912047c42a62bfd1d9d2
MarkLai <icman.tw@gmail.com>
2018-07-02 00:52:21 +0800

    改成用function 形式，沒有重現之前的function 變數未定義的bug...，這次修改反而正常

--- 4/get_largest_plaindrome.php
+++ 4/get_largest_plaindrome.php
@@ -5,37 +5,45 @@
  * Find the largest palindrome made from the product of two 3-digit numbers.
  *
  */
-if(!empty($argv[1])){
-	if($argv[1] < 2 ){
-		return false;
-	}else{
-		$digit = $argv[1];
-	}
-}
 
-$plaindrome = 0;
-$_tmp_plaindrome = 0;
+function maxplaindrome($d){
+	$plaindrome = 0;
+	$_tmp_plaindrome = 0;
 
-$append_zero = str_repeat("0",$digit-1);
-$begin = (int)"1".$append_zero;
-$end = (int)str_repeat("9",$digit);
+	$append_zero = str_repeat("0",$d-1);
+	$begin = (int)"1".$append_zero;
+	$end = (int)str_repeat("9",$d);
 
-for($i=$begin;$i<=$end;$i++){
-	for($j=$begin;$j<=$end;$j++){
-		$k = $i*$j;
-		$_k = str_split($k);
-		$reverse_k =implode("",array_reverse($_k));
+	for($i=$begin;$i<=$end;$i++){
+		for($j=$begin;$j<=$end;$j++){
+			$k = $i*$j;
+			$_k = str_split($k);
+			$reverse_k =implode("",array_reverse($_k));
 
-		if($k == $reverse_k){
-			$_tmp_plaindrome = $k;
-			if($_tmp_plaindrome > $plaindrome){
-				$plaindrome = $_tmp_plaindrome;
+			if($k == $reverse_k){
+				$_tmp_plaindrome = $k;
+				if($_tmp_plaindrome > $plaindrome){
+					$plaindrome = $_tmp_plaindrome;
+				}
 			}
 		}
 	}
+	return $plaindrome;
 }
 
+if(!empty($argv[1])){
+	if($argv[1] < 2 ){
+		echo "digit must greater than 1.";
+		exit;
+	}else{
+		$digit = $argv[1];
+	}
+}else{
+	echo "please tell me a digit for calculate.";
+	exit;
+}
 
-echo "the largest plaindrome is ".$plaindrome."\n";
+$dd = maxplaindrome($digit);
+echo "the largest plaindrome is ".$dd."\n";
 
 
--- 4/readme.md
+++ 4/readme.md
@@ -2,3 +2,4 @@
 * from the product of two 2-digit numbers is 9009 = 91 × 99.
 * 找出來給的位數的 plaindrome 最大乘積及數字
 * 要再改成function ,以及搞懂為何變成FUNCTION時，變數會說沒有定義
+* 已有改成用function 形式，但是沒有重現變數未定義的情況
